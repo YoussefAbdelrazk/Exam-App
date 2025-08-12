0a693dc92e6a3ae4baf51c5bfe9d26cc13bc208b
YoussefAbdelrazk <youssefabdelrazk2002@gmail.com>
2025-08-12 15:13:02 +0300

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export default function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  const isPublicRoute = pathname.startsWith('/login') || pathname.startsWith('/signup');
+  const isPublicRoute =
+    pathname.startsWith('/login') ||
+    pathname.startsWith('/signup') ||
+    pathname.startsWith('/forget-password');
   const token = req.cookies.get('token');
   if (!isPublicRoute && !token) {
     return NextResponse.redirect(new URL('/login', req.url));
