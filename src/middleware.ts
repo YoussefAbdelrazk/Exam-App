80740c9d3493369d41c2b6eddd150883f4451444
YoussefAbdelrazk <youssefabdelrazk2002@gmail.com>
2025-08-12 15:00:03 +0300

    update middlware

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const isPublicRoute = pathname.startsWith('/login') || pathname.startsWith('/signup');
+  const token = req.cookies.get('token');
+  if (!isPublicRoute && !token) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
