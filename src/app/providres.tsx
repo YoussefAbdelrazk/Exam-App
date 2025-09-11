553c34387618e2c440babfb3d7d475251b40c14f
YoussefAbdelrazk <youssefabdelrazk2002@gmail.com>
2025-09-11 20:15:49 +0300

    Refact : Layout.tsx with uninstall not important lib

--- src/app/providres.tsx
+++ src/app/providres.tsx
@@ -2,17 +2,14 @@
 
 import { getQueryClient } from '@/lib/get-query';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'sonner';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const reactQueryClient = getQueryClient();
   return (
-    <SessionProvider>
-      <QueryClientProvider client={reactQueryClient}>
-        {children}
-        <Toaster />
-      </QueryClientProvider>
-    </SessionProvider>
+    <QueryClientProvider client={reactQueryClient}>
+      {children}
+      <Toaster />
+    </QueryClientProvider>
   );
 }
